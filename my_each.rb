50968e435b8e84da91b21287b6dbbca514929e47
Sarah Harrs <sarahharrs@gmail.com>
2019-07-01 12:44:19 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,9 @@ def my_each(collection)
 collection = [1, 2, 3, 4]
 if block_given?
   i = 0
-while i < array.length 
-yield (array[i])
+while i <  collection.length 
+yield (collection[i])
 i = i + 1
 end 
-array
+collection
 end
